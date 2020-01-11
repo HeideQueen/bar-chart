6b0eb63006b2ff43f20f54ddb797108da99eda96
HeideQueen <lucyfabz@gmail.com>
2020-01-11 14:51:06 -0600

    fix async function to use try catch block

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', getData);
 
 async function getData() {
-  const res = await fetch(
-    'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
-  );
+  try {
+    const res = await fetch(
+      'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+    );
 
-  const { data } = await res.json();
+    const { data } = await res.json();
 
-  generateBarChart(data);
+    generateBarChart(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function generateBarChart(data) {
